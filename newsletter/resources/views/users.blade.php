5790e6aa1577492ee745f430f3665fa5f01e3f79
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-02-16 15:30:47 +0100

    select done

--- newsletter/resources/views/users.blade.php
+++ newsletter/resources/views/users.blade.php
@@ -89,7 +89,7 @@
                                         <div class="col-span-2 mx-auto sm:col-span-1">
                                             <select name="role"
                                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                                <option value="{{ $role->id }}" selected="">Select category</option>
+                                                <option value="" selected="">Select category</option>
                                                 @foreach($roles as $role)
                                                     <option
                                                         value="{{ $role->id }}">{{ $role->name }}</option>
